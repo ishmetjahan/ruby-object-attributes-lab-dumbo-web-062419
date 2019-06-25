43618d9b4bf6a4d59c03d95997048d16eb663015
Ishmet <ishmetjahan@yahoo.com>
2019-06-25 15:18:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,19 @@
 class Dog 
   
-  def int (name) 
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   
-  def type_of (breed)
-    @breed = breed
+  def name
+    @name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
   end
   
 end
